067a6d514ed6fb8a951e222d8139c1984b59205c
Jahan S <jshahpro1@gmail.com>
2025-08-17 08:37:58 -0600

    updates to disclaimers

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="site-footer">
         <div class="container">
-            <p>&copy; 2025 myHealth Dashboard POC. Designed and built for demonstration purposes only.</p>
-            <p class="footer-note">All patient data is dummy data for testing.</p>
+            <p>&copy; 2025 myHealth Dashboard POC. Proof-of-concept showcasing potential future-state healthcare interoperability aligned to privacy and data standards.</p>
+            <p class="footer-note">Designed and built for demonstration purposes only. All patient data is dummy data for testing.</p>
         </div>
     </footer>
 </body>
